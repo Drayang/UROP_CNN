1a56e52481d0aa0f48241ba2fa114fa9ca321a52
Drayang <53387856+Drayang@users.noreply.github.com>
2020-12-28 14:35:04 +0800

    Add files via upload

---
+++ Image_processing.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Drayang
+@Supervisor : Dr Soon Foo Chong
+Created on Wed Dec 23 15:12:36 2020
+Image_processing : To process the enormous amount of CompCar images into dataset form
+                   Return: CompCar.npz file consist of x_test,y_test,x_train and y_train dictionary in np.array form 
+
+"""
+
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy import io
+from tensorflow.keras.utils import to_categorical
+from numpy import savez_compressed
+
+
+'''
+
+Load important txt file for image converting purpose
+'''
+
+
+### sv_make_model_name.mat
+sv_make_model_name_mat = io.loadmat('sv_make_model_name.mat')
+sv_make_model_name = sv_make_model_name_mat['sv_make_model_name']
+model_data = np.array(sv_make_model_name[:,1])
+
+### read .txt file to acess the image
+#output is 1-D np.array list
+test_list = np.loadtxt("test_surveillance.txt", comments="#", delimiter=",",dtype = 'str', unpack=False)
+train_list = np.loadtxt('train_surveillance.txt',comments="#", delimiter=",",dtype = 'str', unpack=False)
+
+
+'''
+comment: sv_make_model_name.mat no need here, but i can try to match the model name with the vehicle  
+'''
+
+
+'''
+convert images into data set
+'''
+
+### Load all images in a directory
+x_train = list()
+y_train = list()
+
+x_test = list()
+y_test = list()
+
+
+# Argument i is to control the number of image, remove it to convert all images
+def creating_data(path_list,x_data,y_data,i):
+    for img in path_list:
+        #load image
+        image = Image.open('image/'+ img)
+        
+        ## resize image
+        #image.thumbnail((32,32),Image.ANTIALIAS)
+        image = image.resize((32,32),Image.ANTIALIAS)
+        work_data = np.asarray(image)
+        #work_data = work_data/255.0  #normalization
+        
+        
+        #To get the Make of vehicle indices
+        index = img.split('/')
+        y_data.append(int(index[0])-1)
+        #return index that indicate which vehicle make it belong to
+        
+        #plt.imshow(work_data)
+        x_data.append(work_data)
+        # return 3D array matrix
+        print('> loaded {} {}' .format(i, img))
+        
+        i = i- 1
+    
+        if i == 0:
+            break
+
+'''
+Comment : Can remove /255.0 so data is in normal form so it is exactly the same 
+          format as online data set source
+          Besides, normalization causing data store as float64 which use out too much memory (cant even process
+          100 class)
+'''
+
+
+## Creating train data
+# 897:10 class;5060:50 class 12036:100 class;31148: 281 class
+i = 5060
+creating_data(train_list,x_train,y_train,i)
+x_train = np.array(x_train)
+print("x_train done")
+
+## Creating test data
+# 386:10 class; 2166 : 50 class; 5154:100 class;13333:281 class
+i = 2166
+creating_data(test_list,x_test,y_test,i)
+x_test = np.array(x_test)
+
+print("x_test done")
+savez_compressed('Car50.npz',x_test = x_test,y_test = y_test, x_train = x_train, y_train = y_train)
+
+print('Saving done')
+'''
+
+Comment: y_train and y_test no necessary to convert into np.array form (can in list data type)
+         We will do to_categorical when load out the y data and y data will become 2D np.array 
+ytainisa2dD
+'''
+
+
+'''
+extracting data set(for checking purpose only)
+
+'''
+
+#savez_compressed('Comar.npz', x_train = x_train,y_train = y_train)
+
+# # load numpy array from .npz file
+# # load dict of arrays
+# dict_data = np.load('CompCar.npz')
+
+# # extract the first array
+# datax = dict_data['x_train']
+# datay = dict_data['y_train']
+# # print the array
+# print(datax[449])
+# print(datay[449])
+# plt.imshow(datax[449])
+
+# #check model name correct or not
+# datay = to_categorical(datay)
+# print(model_data[np.argmax(datay[449])])
+
+'''
+Older version used.
+'''
+
+#print('X_data is:')
+#print(x_test[:])
+# print('y_data is:')
+# print(y_test[0])
+
+
+# for img in train_list:
+#     #load image
+#     image = Image.open('image/'+ img)
+#     train_data = np.asarray(image)
+#     train_data = train_data/255.0
+    
+#     #To get the Make of vehicle indices
+#     model_index = img.split('/')
+#     y_train.append(int(model_index[0])-1)
+    
+#     # this one give string
+#     #y_train.append(make_data[ int(model_index[0])-1 ])  #To get the correct indices
+    
+#     #print(x_train)
+#     #print(x_train.shape)
+    
+#     ## resize image
+#     image.thumbnail((640,640))
+#     print(image.size)
+    
+#     ## show image using Plot
+#     plt.imshow(train_data)
+    
+#     ## show image using image app
+#     #image.show()
+    
+#     #print('> loaded {} {}' .format(i, x_train.shape))
+    
+#     x_train.append(train_data)
+#     break
+
+
+
+
+
+
+
+'''
+
+Example found online ( can remove )
+
+'''
+
+# import os 
+# import cv2
+
+# #setting the path to the directory containing the pics
+# path = 'testing/'
+
+#print(os.listdir())
+
+#appending the pics to the training data list
+# training_data = []
+# for img in os.listdir(path):
+#     pic = cv2.imread(os.path.join(path,img))
+#     pic = cv2.cvtColor(pic,cv2.COLOR_BGR2RGB)
+#     pic = cv2.resize(pic,(80,80))
+#     training_data.append([pic])
+    
+    
+# #converting the list to numpy array and saving it to a file using #numpy.save
+# np.save(os.path.join(path,'features'),np.array(training_data))
+
+
+# #loading the saved file once again
+# saved = np.load(os.path.join(path,'features.npy'))
+
+# print(saved)
+
+# plt.imshow(saved[0].reshape(80,80,3))
+# plt.imshow(np.array(training_data[0]).reshape(80,80,3))
---
+++ Trial_2.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Drayang
+@Supervisor : Dr Soon Foo Chong
+Created on :  Sun Dec 27 13:27:03 2020
+Trial_2
+"""
+
+
+# Import required module 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers, models,losses
+from scipy import io
+
+import random
+
+'''
+Load .mat file text
+'''
+### color_list.mat
+color_list_mat = io.loadmat('color_list.mat')
+color_list = color_list_mat['color_list']
+image_path = np.array(color_list[:,0])
+color_value = np.array(color_list[:,1])
+
+color =['black','white','red','yellow','blue','green','purple','brown','magenta','silver','aqua'] 
+
+### sv_make_model_name.mat
+sv_make_model_name_mat = io.loadmat('sv_make_model_name.mat')
+sv_make_model_name = sv_make_model_name_mat['sv_make_model_name']
+make_name = np.array(sv_make_model_name[:,0])
+model_name = np.array(sv_make_model_name[:,1])
+model_id_data = np.array(sv_make_model_name[:,2])
+
+
+
+
+
+'''
+Comment : make_data and model_id_data are not necessary here, but just leave it there
+          color_list.mat: haven't find out the purpose of color yet, maybe use for plotting or segmentation purpose
+'''
+
+### read .txt file to acess the image
+#output is 1-D np.array list
+test_list = np.loadtxt("test_surveillance.txt", comments="#", delimiter=",",dtype = 'str', unpack=False)
+train_list = np.loadtxt('train_surveillance.txt',comments="#", delimiter=",",dtype = 'str', unpack=False)
+
+
+'''
+Load data set
+'''
+
+dataset_file = 'Car10.npz'
+
+## load numpy array from .npz file
+def load_data(file):
+    # load numpy array from .npz file
+    # load dict of arrays
+    dict_data = np.load(file)
+    
+    # extract the array
+    x_test, y_test = dict_data['x_test'],dict_data['y_test']
+    x_train , y_train = dict_data['x_train'], dict_data['y_train']
+    
+    #return np.array
+    return (x_train,y_train), (x_test,y_test)
+
+(x_train,y_train), (x_test,y_test) = load_data(dataset_file)
+ 
+#Normalize the pixel value
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') /255.0
+
+#Identify the number of class
+class_num = max(y_test)+1
+
+
+## np.argmax use to find the index of max value, hence use to identify the model name
+#print(model_data[np.argmax(y_train)])
+
+'''
+Comment: X_train and x_test is a 4D np.array
+'''
+
+'''
+Verify the dataset
+'''
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    index= i*i
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[index], cmap=plt.cm.binary)
+    plt.xlabel(model_name[y_train[index]])
+plt.show()
+
+#%%
+'''
+Designing the model
+'''
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=x_train.shape[1:]))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(class_num))
+
+
+# model.summary()
+
+# Compile and train model
+
+model.compile(optimizer='adam',
+              loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+'''
+Training data
+'''
+epochs = 10
+history = model.fit(x_train, y_train, epochs=epochs, 
+                    validation_data=(x_test, y_test))
+
+## Evaluate the model
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='upper right')
+
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+
+#%%
+'''
+Prediction 
+'''
+
+probability_model = tf.keras.Sequential([model, layers.Softmax()])
+
+rand = random.randint(0,386)
+
+#prediction is an array of "confidence" to the class
+predictions = probability_model.predict(x_test)
+print(predictions[rand])
+prediction_index = np.argmax(predictions[rand])
+prediction_model_name = model_name[prediction_index]
+correct_model_name = model_name[y_test[rand]]
+print('The prediction car model:{}'.format(prediction_model_name))
+print('The correct car model:{}'.format(correct_model_name))
+
+#%%
